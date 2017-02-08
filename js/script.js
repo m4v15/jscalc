6f0f3c82d1f02f302a89bb93dfbdc5a969bede5c
Matt Davis <matthewpeterdavis@gmail.com>
2017-02-08 17:44:48 +0000

    Begin basic javascript file
    firstly get array of buttons and then create a function for click event, which is then acted on using a for loop and addEventListener

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
 //comment
+window.onload = function(){
+
+var keys = document.querySelectorAll('#calc span');
+var screenface = document.getElementById('screen');
+
+var buttonclick = function(e){
+  screenface.innerHTML = this.innerHTML;
+
+};
+
+for (var i=0; i<keys.length;i++){
+  console.log(keys[i]);
+  keys[i].addEventListener('click',buttonclick, false);
+};
+
+}
