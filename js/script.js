867941688f86afd38e151ac434932db054c0ce63
Matt Davis <matthewpeterdavis@gmail.com>
2017-02-11 17:30:50 +0000

    Add clear button functionality
    also changed some styles

--- js/script.js
+++ js/script.js
@@ -2,25 +2,43 @@
 window.onload = function(){
 
 var keys = document.querySelectorAll('#calc span');
-var screenface = document.getElementById('screen1');
-var miniscreenface = document.getElementById('screen2');
+var screen1 = document.getElementById('screen1');
+var screen2 = document.getElementById('screen2');
 var theSum='';
 
-var buttonclick = function(e){
-  if (this.innerHTML!=='='){
-  screenface.innerHTML = this.innerHTML;
-  miniscreenface.innerHTML += this.innerHTML;
+//FIX THIS:
+//Clear buttonclick DONE
+//decimal point twice
+//pressing a number after the calculation should just clear as well
+//can't press two operators in a row
 
-  theSum +=this.innerHTML;
-};
+var buttonclick = function(e){
+
+  //clear screen on C
+  if (this.innerHTML==='C'){
+    screen1.innerHTML='';
+    screen2.innerHTML='';
+    theSum='';
+  };
+
+
+//when not C or Equals are pressed:
+  if (this.className!=='func'){
+  screen1.innerHTML = this.innerHTML; //replace characters on main screen
+  screen2.innerHTML += this.innerHTML;  //add to characters on secondary
+
+  theSum +=this.innerHTML; //add to theSum variable
+  };
 if (this.innerHTML==='='&&(theSum)){
-    screenface.innerHTML=eval(theSum);
+    console.log(theSum);
+    screen1.innerHTML=eval(theSum);
     theSum = eval(theSum);
+    screen2.innerHTML = eval(theSum);
+    console.log(theSum);
   };
 };
 
 for (var i=0; i<keys.length;i++){
-  console.log(keys[i]);
   keys[i].addEventListener('click',buttonclick, false);
 };
 
