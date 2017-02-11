0d12bafb930882d2eee3dc9254ac6c2e7050fcae
Matt Davis <matthewpeterdavis@gmail.com>
2017-02-11 18:29:40 +0000

    Add decimal point button working functionality
    made it only possible to press the point once during a number

--- js/script.js
+++ js/script.js
@@ -5,12 +5,15 @@ var keys = document.querySelectorAll('#calc span');
 var screen1 = document.getElementById('screen1');
 var screen2 = document.getElementById('screen2');
 var theSum='';
+var decimalpressed=false;
+var lastbutton
 
 //FIX THIS:
 //Clear buttonclick DONE
-//decimal point twice
+//decimal point twice DONE
 //pressing a number after the calculation should just clear as well
 //can't press two operators in a row
+//something about negative numbers
 
 var buttonclick = function(e){
 
@@ -18,23 +21,43 @@ var buttonclick = function(e){
   if (this.innerHTML==='C'){
     screen1.innerHTML='';
     screen2.innerHTML='';
-    theSum='';
+    theSum=0;
+    lastbutton=this;
   };
 
+  //when press decimal
+  if (this.innerHTML==='.'&&(decimalpressed===false)){
+    screen1.innerHTML = this.innerHTML; //replace characters on main screen
+    screen2.innerHTML += this.innerHTML;  //add to characters on secondary
 
-//when not C or Equals are pressed:
+    theSum +=this.innerHTML; //add to theSum variable
+    lastbutton=this;
+    decimalpressed=true;
+  }
+
+//what happens when numbers or operators are pressed
   if (this.className!=='func'){
-  screen1.innerHTML = this.innerHTML; //replace characters on main screen
-  screen2.innerHTML += this.innerHTML;  //add to characters on secondary
+    screen1.innerHTML = this.innerHTML; //replace characters on main screen
+    screen2.innerHTML += this.innerHTML;  //add to characters on secondary
 
-  theSum +=this.innerHTML; //add to theSum variable
+    theSum +=this.innerHTML; //add to theSum variable
+    lastbutton=this;
+    if (this.className==='op'){decimalpressed=false}; //revert decimalpressed to false after op
   };
-if (this.innerHTML==='='&&(theSum)){
+
+
+
+
+//what happens when equals is pressed when there is a sum to solve
+  if (this.id==='equals'&&(theSum)){
     console.log(theSum);
-    screen1.innerHTML=eval(theSum);
     theSum = eval(theSum);
-    screen2.innerHTML = eval(theSum);
+    screen1.innerHTML=theSum;
+    screen2.innerHTML = theSum;
     console.log(theSum);
+    lastbutton=this;
+    decimalpressed=false;
+
   };
 };
 
