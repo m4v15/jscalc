e16cf92e4e3b88820af79399a8eedc976e859754
Matt Davis <matthewpeterdavis@gmail.com>
2017-02-11 14:47:31 +0000

    Add basic calculator functionality
    Can now do sums and things - changed x to * in the html file for this to
    work and used eval in js file.
    
    Need to now maybe add smaller screen showing sum and sort out
    decimal point and C button issue, could then add memory functionality
    and also style it all better and nicer looking.

--- js/script.js
+++ js/script.js
@@ -3,10 +3,17 @@ window.onload = function(){
 
 var keys = document.querySelectorAll('#calc span');
 var screenface = document.getElementById('screen');
+var theSum='';
 
 var buttonclick = function(e){
+  if (this.innerHTML!=='='){
   screenface.innerHTML = this.innerHTML;
-
+  theSum +=this.innerHTML;
+};
+if (this.innerHTML==='='&&(theSum)){
+    screenface.innerHTML=eval(theSum);
+    theSum = eval(theSum);
+  };
 };
 
 for (var i=0; i<keys.length;i++){
